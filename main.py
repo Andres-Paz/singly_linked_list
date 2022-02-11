eb0b0fe041bedcc33ece5e8a8f8633a026e5d9b8
Andres-Paz <marco.andres.paz@icloud.com>
2022-02-10 21:44:22 -0600

    Tarea metodo recorrer

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ numeros.insertar_inicio(14)
 numeros.insertar_inicio(102)
 numeros.insertar_inicio(48)
 
-recorrer = numeros.recorrer()
-print(recorrer)
+resultado = numeros.recorrer()
+print(resultado)
 
 
